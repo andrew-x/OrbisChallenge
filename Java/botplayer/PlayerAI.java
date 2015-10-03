e98709f6ad896c5d2d3ccb6db33bf71f6fb8155e
AndrewXia <andrew.xia@live.ca>
2015-10-03 14:43:30 -0400

    Created Generate Files

--- Java/botplayer/PlayerAI.java
+++ Java/botplayer/PlayerAI.java
@@ -1,15 +1,35 @@
+import com.orbischallenge.engine.gameboard.GameObject;
+import com.orbischallenge.engine.gameboard.Gameboard;
+
 import java.util.*;
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class PlayerAI extends ClientAI {
+
+    private ArrayList<GameObjects> leftFile;
+    private ArrayList<GameObjects> rightFile;
+    private ArrayList<GameObject> upFile;
+    private ArrayList<GameObject> downFile;
+
 	public PlayerAI() {
-		//Write your initialization here
+        leftFile = new ArrayList<>();
+        rightFile = new ArrayList<>();
+        upFile = new ArrayList<>();
+        downFile = new ArrayList<>();
 	}
 
 	@Override
 	public Move getMove(Gameboard gameboard, Opponent opponent, Player player) throws NoItemException, MapOutOfBoundsException {
-		
-		//Write your AI here
+
 		return Move.NONE;
 	}
+
+    private void populateFiles(Gameboard gameboard){
+        int startX = 0;
+        int startY = 0;
+        for (int x = startX; x < gameboard.getWidth(); i++){
+        }
+
+    }
 }
